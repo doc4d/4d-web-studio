ab6c1ae3da2891b522f7df3b30e78fee774205b5
Tony Briget <80901494+tony-b-4d@users.noreply.github.com>
2021-09-24 16:20:08 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "doc4d", // Usually your GitHub org/user name.
   projectName: "4d-web-studio", // Usually your repo name.
-  noIndex: false,
+  noIndex: true,
   themeConfig: {
     prism: {
       theme: require("./src/theme/highlight/themes/palenight"),
