b913a2cc08ba4363f55edeea3b31ee56aadcee94
Tony Briget <tony.briget@4d.com>
2021-10-04 16:56:16 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,7 +24,7 @@ const features = [
     imageUrl: "img/wysiwig.png",
     description: (
       <>
-        Design professional, responsive, and custom web applications using a visual canvas.
+        Design professional and custom web applications using a visual canvas.
 
       </>
     ),
